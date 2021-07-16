4e88a9d23c5ed5127884e58820d6e9a6e0930871
joão marmentini <joao.marmentini@edu.pucrs.br>
2021-07-16 00:02:13 -0300

    std repo

---
+++ registration/src/main/java/pucrs/jvzmarmentini/registration/StudentRepository.java
@@ -0,0 +1,6 @@
+package pucrs.jvzmarmentini.registration;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, String> {
+}
