be4ed18ec55aaa20019ee670419fcfba8c7f808b
joão marmentini <joao.marmentini@edu.pucrs.br>
2021-07-19 14:27:03 -0300

    DTOs

---
+++ registration/src/main/java/pucrs/jvzmarmentini/registration/application/dto/SubscribersTotalDTO.java
@@ -0,0 +1,14 @@
+package pucrs.jvzmarmentini.registration.application.dto;
+
+public class SubscribersTotalDTO {
+
+    private int totalSubscribers;
+
+    public SubscribersTotalDTO(int totalSubscribers) {
+        this.totalSubscribers = totalSubscribers;
+    }
+
+    public int getTotalSubscribers() {
+        return totalSubscribers;
+    }
+}
